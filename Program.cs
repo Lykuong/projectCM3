bbaf6870ba99e813dd5a43860d87530646ac47e9
Lykuong <40283730+Lykuong@users.noreply.github.com>
2018-06-15 03:11:00 +0700

    frist project
    first Project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CShapeFile
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               FileStream fstr;
+               try
+               {
+                    fstr = new FileStream("data.txt.txt", FileMode.Open);
+               }
+               catch(IOException ecx)
+               {
+                    Console.Write(ecx.Message);
+                    return;
+               }
+          }
+     }
+}
